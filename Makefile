bb9cf50f0803be2c1f0ac92055e53cfabf4b433e
Taner Öksüz <taner.oeksuez@outlook.de>
2023-02-18 16:10:57 +0100

    scripts and make file optimized. submodule updated. error by handling of exception fixed.

--- Makefile
+++ Makefile
@@ -1,30 +1,27 @@
 default: none
 
-VERILATOR ?= /opt/verilator/bin/verilator
-SYSTEMC ?= /opt/systemc
-RISCVDV ?= /opt/riscv-dv
-RISCV ?= /opt/rv32imfcb/bin/riscv32-unknown-elf-
-MARCH ?= rv32imfc_zba_zbb_zbc_zbs_zicsr_zifencei
-MABI ?= ilp32f
-ITER ?= 10
-CSMITH ?= /opt/csmith
-CSMITH_INCL ?= $(shell ls -d $(CSMITH)/include/csmith-* | head -n1)
-GCC ?= /usr/bin/gcc
-PYTHON ?= /usr/bin/python3
-BASEDIR ?= $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
-OVP_BIT ?= riscv-ovpsim-plus-bitmanip-tests.zip
-OVP_FP ?= riscv-ovpsim-plus-fp-tests.zip
-OFFSET ?= 0x100000 # Number of dwords in blockram (address range is OFFSET * 8)
-PROGRAM ?= dhrystone
-AAPG ?= aapg
-CYCLES ?= 10000000000
-FPGA ?= quartus # tb vivado quartus
-WAVE ?= off # "on" for saving dump file
+export VERILATOR ?= /opt/verilator/bin/verilator
+export SYSTEMC ?= /opt/systemc
+export RISCVDV ?= /opt/riscv-dv
+export RISCV ?= /opt/rv32imfcb/bin/riscv32-unknown-elf-
+export MARCH ?= rv32imfc_zba_zbb_zbc_zbs_zicsr_zifencei
+export MABI ?= ilp32f
+export ITER ?= 10
+export CSMITH ?= /opt/csmith
+export GCC ?= /usr/bin/gcc
+export PYTHON ?= /usr/bin/python3
+export BASEDIR ?= $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
+export OFFSET ?= 0x100000# Number of dwords in blockram (address range is OFFSET * 8)
+export PROGRAM ?= dhrystone# aapg compliance coremark csmith dhrystone isa riscv-dv sram timer uart
+export AAPG ?= aapg
+export CYCLES ?= 10000000000
+export FPGA ?= quartus# tb vivado quartus
+export WAVE ?= off# "on" for saving dump file
 
 generate:
-	soft/compile.sh --riscv ${RISCV} --march ${MARCH} --mabi ${MABI} --iter ${ITER} --python ${PYTHON} --offset ${OFFSET} --basedir ${BASEDIR} --aapg ${AAPG} --ovp-bit ${OVP_BIT} --ovp-fp ${OVP_FP} --csmith ${CSMITH} --csmith_incl ${CSMITH_INCL} --riscvdv ${RISCVDV} --gcc ${GCC} --program ${PROGRAM}
+	soft/compile.sh
 
 simulate:
-	sim/run.sh --basedir ${BASEDIR} --verilator ${VERILATOR} --systemc ${SYSTEMC} --program ${PROGRAM} --cycles ${CYCLES} --wave ${WAVE}
+	sim/run.sh
 
 all: generate simulate
--- sim/run.sh
+++ sim/run.sh
@@ -1,49 +1,11 @@
 #!/bin/bash
 set -e
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    --basedir) 
-      DIR="$2"
-      shift
-      shift
-      ;;
-    --verilator)
-      VERILATOR="$2"
-      shift
-      shift
-      ;;
-    --systemc)
-      SYSTEMC="$2"
-      shift
-      shift
-      ;;
-    --program)
-      PROGRAM="$2"
-      shift
-      shift
-      ;;
-    --cycles)
-      CYCLES="$2"
-      shift
-      shift
-      ;;
-    --wave)
-      WAVE="$2"
-      shift
-      shift
-      ;;
-    *)
-      echo "Unknown commandline arguments: $1 -> $2"
-      exit 1
-  esac
-done
-
-if [ ! -d "$DIR/sim/work" ]; then
-  mkdir $DIR/sim/work
+if [ ! -d "$BASEDIR/sim/work" ]; then
+  mkdir $BASEDIR/sim/work
 fi
 
-rm -rf $DIR/sim/work/*
+rm -rf $BASEDIR/sim/work/*
 
 export SYSTEMC_LIBDIR=$SYSTEMC/lib-linux64/
 export SYSTEMC_INCLUDE=$SYSTEMC/include/
@@ -53,121 +15,102 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-cd $DIR/sim/work
+cd $BASEDIR/sim/work
 
 start=`date +%s`
 if [ "$WAVE" = 'on' ]
 then
-	${VERILATOR} --sc -Wno-UNOPTFLAT -Wno-UNSIGNED --trace -trace-max-array 128 --trace-structs -f ${DIR}/sim/files.f --top-module soc --exe ${DIR}/verilog/tb/soc.cpp
+	${VERILATOR} --sc -Wno-UNOPTFLAT -Wno-UNSIGNED --trace -trace-max-array 128 --trace-structs -f $BASEDIR/sim/files.f --top-module soc --exe $BASEDIR/verilog/tb/soc.cpp
 	make -s -j -C obj_dir/ -f Vsoc.mk Vsoc
   if [ "$PROGRAM" = 'dhrystone' ]
   then
-    cp $DIR/build/dhrystone/dat/dhrystone.dat bram.dat
-    cp $DIR/build/dhrystone/elf/dhrystone.host host.dat
+    cp $BASEDIR/build/dhrystone/dat/dhrystone.dat bram.dat
+    cp $BASEDIR/build/dhrystone/elf/dhrystone.host host.dat
   	obj_dir/Vsoc $CYCLES dhrystone 2> /dev/null
   elif [ "$PROGRAM" = 'whetstone' ]
   then
-    cp $DIR/build/whetstone/dat/whetstone.dat bram.dat
-    cp $DIR/build/whetstone/elf/whetstone.host host.dat
+    cp $BASEDIR/build/whetstone/dat/whetstone.dat bram.dat
+    cp $BASEDIR/build/whetstone/elf/whetstone.host host.dat
   	obj_dir/Vsoc $CYCLES whetstone 2> /dev/null
   elif [ "$PROGRAM" = 'coremark' ]
   then
-    cp $DIR/build/coremark/dat/coremark.dat bram.dat
-    cp $DIR/build/coremark/elf/coremark.host host.dat
+    cp $BASEDIR/build/coremark/dat/coremark.dat bram.dat
+    cp $BASEDIR/build/coremark/elf/coremark.host host.dat
   	obj_dir/Vsoc $CYCLES coremark 2> /dev/null
   elif [ "$PROGRAM" = 'aapg' ]
   then
-    cp $DIR/build/aapg/dat/aapg.dat bram.dat
-    cp $DIR/build/aapg/elf/aapg.host host.dat
+    cp $BASEDIR/build/aapg/dat/aapg.dat bram.dat
+    cp $BASEDIR/build/aapg/elf/aapg.host host.dat
   	obj_dir/Vsoc $CYCLES aapg 2> /dev/null
   elif [ "$PROGRAM" = 'riscv-dv' ]
   then
-    cp $DIR/build/riscv-dv/dat/riscv-dv.dat bram.dat
-    cp $DIR/build/riscv-dv/elf/riscv-dv.host host.dat
+    cp $BASEDIR/build/riscv-dv/dat/riscv-dv.dat bram.dat
+    cp $BASEDIR/build/riscv-dv/elf/riscv-dv.host host.dat
   	obj_dir/Vsoc $CYCLES riscv-dv 2> /dev/null
   elif [ "$PROGRAM" = 'csmith' ]
   then
-    cp $DIR/build/csmith/dat/csmith.dat bram.dat
-    cp $DIR/build/csmith/elf/csmith.host host.dat
+    cp $BASEDIR/build/csmith/dat/csmith.dat bram.dat
+    cp $BASEDIR/build/csmith/elf/csmith.host host.dat
   	obj_dir/Vsoc $CYCLES csmith 2> /dev/null
   elif [ "$PROGRAM" = 'torture' ]
   then
-    cp $DIR/build/torture/dat/torture.dat bram.dat
-    cp $DIR/build/torture/elf/torture.host host.dat
+    cp $BASEDIR/build/torture/dat/torture.dat bram.dat
+    cp $BASEDIR/build/torture/elf/torture.host host.dat
   	obj_dir/Vsoc $CYCLES torture 2> /dev/null
   elif [ "$PROGRAM" = 'uart' ]
   then
-    cp $DIR/build/uart/dat/uart.dat bram.dat
-    cp $DIR/build/uart/elf/uart.host host.dat
+    cp $BASEDIR/build/uart/dat/uart.dat bram.dat
+    cp $BASEDIR/build/uart/elf/uart.host host.dat
   	obj_dir/Vsoc $CYCLES uart 2> /dev/null
   elif [ "$PROGRAM" = 'timer' ]
   then
-    cp $DIR/build/timer/dat/timer.dat bram.dat
-    cp $DIR/build/timer/elf/timer.host host.dat
+    cp $BASEDIR/build/timer/dat/timer.dat bram.dat
+    cp $BASEDIR/build/timer/elf/timer.host host.dat
   	obj_dir/Vsoc $CYCLES timer 2> /dev/null
   elif [ "$PROGRAM" = 'sram' ]
   then
-    cp $DIR/build/sram/dat/sram.dat bram.dat
-    cp $DIR/build/sram/elf/sram.host host.dat
+    cp $BASEDIR/build/sram/dat/sram.dat bram.dat
+    cp $BASEDIR/build/sram/elf/sram.host host.dat
   	obj_dir/Vsoc $CYCLES sram 2> /dev/null
   elif [ "$PROGRAM" = 'compliance' ]
   then
-    for filename in $DIR/build/compliance/dat/*.dat; do
+    for filename in $BASEDIR/build/compliance/dat/*.dat; do
       cp $filename bram.dat
       filename=${filename##*/}
       filename=${filename%.dat}
-      cp $DIR/build/compliance/elf/${filename}.host host.dat
+      cp $BASEDIR/build/compliance/elf/${filename}.host host.dat
       echo "${filename}"
     	obj_dir/Vsoc $CYCLES ${filename} 2> /dev/null
-    done
-  elif [ "$PROGRAM" = 'ovp' ]
-  then
-    for filename in $DIR/build/ovp/dat/*.dat; do
-      cp $filename bram.dat
-      filename=${filename##*/}
-      filename=${filename%.dat}
-      cp $DIR/build/ovp/elf/${filename}.host host.dat
-      cp $DIR/build/ovp/elf/${filename}.begin_signature begin_signature.dat
-      cp $DIR/build/ovp/elf/${filename}.end_signature end_signature.dat
-      cp $DIR/build/ovp/ref/${filename}.reference_output reference.dat
-      echo "${filename}"
-    	obj_dir/Vsoc $CYCLES ${filename} 2> /dev/null
-      if [ "$(diff --color reference.dat signature.dat)" != "" ]
-      then
-        echo "${red}RESULTS NOT OK${reset}"
-      else
-        echo "${green}RESULTS OK${reset}"
-      fi
     done
   elif [ "$PROGRAM" = 'isa' ]
   then
-    for filename in $DIR/build/isa/dat/*.dat; do
+    for filename in $BASEDIR/build/isa/dat/*.dat; do
       cp $filename bram.dat
       filename=${filename##*/}
       filename=${filename%.dat}
-      cp $DIR/build/isa/elf/${filename}.host host.dat
+      cp $BASEDIR/build/isa/elf/${filename}.host host.dat
       echo "${filename}"
     	obj_dir/Vsoc $CYCLES ${filename} 2> /dev/null
     done
   else
-    cp $DIR/$PROGRAM bram.dat
+    cp $BASEDIR/$PROGRAM bram.dat
     filename="$PROGRAM"
     dirname="$PROGRAM"
     filename=${filename##*/}
     filename=${filename%.dat}
     subpath=${dirname%/dat*}
-    cp $DIR/${subpath}/elf/${filename}.host host.dat
-    if [ -e $DIR/${subpath}/elf/${filename}.begin_signature ]
+    cp $BASEDIR/${subpath}/elf/${filename}.host host.dat
+    if [ -e $BASEDIR/${subpath}/elf/${filename}.begin_signature ]
     then
-      cp $DIR/${subpath}/elf/${filename}.begin_signature begin_signature.dat
+      cp $BASEDIR/${subpath}/elf/${filename}.begin_signature begin_signature.dat
     fi
-    if [ -e $DIR/${subpath}/elf/${filename}.end_signature ]
+    if [ -e $BASEDIR/${subpath}/elf/${filename}.end_signature ]
     then
-      cp $DIR/${subpath}/elf/${filename}.end_signature end_signature.dat
+      cp $BASEDIR/${subpath}/elf/${filename}.end_signature end_signature.dat
     fi
-    if [ -e $DIR/${subpath}/ref/${filename}.reference_output ]
+    if [ -e $BASEDIR/${subpath}/ref/${filename}.reference_output ]
     then
-      cp $DIR/${subpath}/ref/${filename}.reference_output reference.dat
+      cp $BASEDIR/${subpath}/ref/${filename}.reference_output reference.dat
     fi
     obj_dir/Vsoc $CYCLES ${filename} 2> /dev/null
     if [ -f "reference.dat" ]
@@ -181,116 +124,97 @@ then
     fi
   fi
 else
-	${VERILATOR} --sc -Wno-UNOPTFLAT -Wno-UNSIGNED -f ${DIR}/sim/files.f --top-module soc --exe ${DIR}/verilog/tb/soc.cpp
+	${VERILATOR} --sc -Wno-UNOPTFLAT -Wno-UNSIGNED -f $BASEDIR/sim/files.f --top-module soc --exe $BASEDIR/verilog/tb/soc.cpp
 	make -s -j -C obj_dir/ -f Vsoc.mk Vsoc
   if [ "$PROGRAM" = 'dhrystone' ]
   then
-    cp $DIR/build/dhrystone/dat/dhrystone.dat bram.dat
-    cp $DIR/build/dhrystone/elf/dhrystone.host host.dat
+    cp $BASEDIR/build/dhrystone/dat/dhrystone.dat bram.dat
+    cp $BASEDIR/build/dhrystone/elf/dhrystone.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'whetstone' ]
   then
-    cp $DIR/build/whetstone/dat/whetstone.dat bram.dat
-    cp $DIR/build/whetstone/elf/whetstone.host host.dat
+    cp $BASEDIR/build/whetstone/dat/whetstone.dat bram.dat
+    cp $BASEDIR/build/whetstone/elf/whetstone.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'coremark' ]
   then
-    cp $DIR/build/coremark/dat/coremark.dat bram.dat
-    cp $DIR/build/coremark/elf/coremark.host host.dat
+    cp $BASEDIR/build/coremark/dat/coremark.dat bram.dat
+    cp $BASEDIR/build/coremark/elf/coremark.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'aapg' ]
   then
-    cp $DIR/build/aapg/dat/aapg.dat bram.dat
-    cp $DIR/build/aapg/elf/aapg.host host.dat
+    cp $BASEDIR/build/aapg/dat/aapg.dat bram.dat
+    cp $BASEDIR/build/aapg/elf/aapg.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'riscv-dv' ]
   then
-    cp $DIR/build/riscv-dv/dat/riscv-dv.dat bram.dat
-    cp $DIR/build/riscv-dv/elf/riscv-dv.host host.dat
+    cp $BASEDIR/build/riscv-dv/dat/riscv-dv.dat bram.dat
+    cp $BASEDIR/build/riscv-dv/elf/riscv-dv.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'csmith' ]
   then
-    cp $DIR/build/csmith/dat/csmith.dat bram.dat
-    cp $DIR/build/csmith/elf/csmith.host host.dat
+    cp $BASEDIR/build/csmith/dat/csmith.dat bram.dat
+    cp $BASEDIR/build/csmith/elf/csmith.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'torture' ]
   then
-    cp $DIR/build/torture/dat/torture.dat bram.dat
-    cp $DIR/build/torture/elf/torture.host host.dat
+    cp $BASEDIR/build/torture/dat/torture.dat bram.dat
+    cp $BASEDIR/build/torture/elf/torture.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'uart' ]
   then
-    cp $DIR/build/uart/dat/uart.dat bram.dat
-    cp $DIR/build/uart/elf/uart.host host.dat
+    cp $BASEDIR/build/uart/dat/uart.dat bram.dat
+    cp $BASEDIR/build/uart/elf/uart.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'timer' ]
   then
-    cp $DIR/build/timer/dat/timer.dat bram.dat
-    cp $DIR/build/timer/elf/timer.host host.dat
+    cp $BASEDIR/build/timer/dat/timer.dat bram.dat
+    cp $BASEDIR/build/timer/elf/timer.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'sram' ]
   then
-    cp $DIR/build/sram/dat/sram.dat bram.dat
-    cp $DIR/build/sram/elf/sram.host host.dat
+    cp $BASEDIR/build/sram/dat/sram.dat bram.dat
+    cp $BASEDIR/build/sram/elf/sram.host host.dat
   	obj_dir/Vsoc $CYCLES 2> /dev/null
   elif [ "$PROGRAM" = 'compliance' ]
   then
-    for filename in $DIR/build/compliance/dat/*.dat; do
+    for filename in $BASEDIR/build/compliance/dat/*.dat; do
       cp $filename bram.dat
       filename=${filename##*/}
       filename=${filename%.dat}
-      cp $DIR/build/compliance/elf/${filename}.host host.dat
+      cp $BASEDIR/build/compliance/elf/${filename}.host host.dat
       echo "${filename}"
     	obj_dir/Vsoc $CYCLES 2> /dev/null
-    done
-  elif [ "$PROGRAM" = 'ovp' ]
-  then
-    for filename in $DIR/build/ovp/dat/*.dat; do
-      cp $filename bram.dat
-      filename=${filename##*/}
-      filename=${filename%.dat}
-      cp $DIR/build/ovp/elf/${filename}.host host.dat
-      cp $DIR/build/ovp/elf/${filename}.begin_signature begin_signature.dat
-      cp $DIR/build/ovp/elf/${filename}.end_signature end_signature.dat
-      cp $DIR/build/ovp/ref/${filename}.reference_output reference.dat
-      echo "${filename}"
-    	obj_dir/Vsoc $CYCLES 2> /dev/null
-      if [ "$(diff --color reference.dat signature.dat)" != "" ]
-      then
-        echo "${red}RESULTS NOT OK${reset}"
-      else
-        echo "${green}RESULTS OK${reset}"
-      fi
     done
   elif [ "$PROGRAM" = 'isa' ]
   then
-    for filename in $DIR/build/isa/dat/*.dat; do
+    for filename in $BASEDIR/build/isa/dat/*.dat; do
       cp $filename bram.dat
       filename=${filename##*/}
       filename=${filename%.dat}
-      cp $DIR/build/isa/elf/${filename}.host host.dat
+      cp $BASEDIR/build/isa/elf/${filename}.host host.dat
       echo "${filename}"
     	obj_dir/Vsoc $CYCLES 2> /dev/null
     done
   else
-    cp $DIR/$PROGRAM bram.dat
+    cp $BASEDIR/$PROGRAM bram.dat
     filename="$PROGRAM"
     dirname="$PROGRAM"
     filename=${filename##*/}
     filename=${filename%.dat}
     subpath=${dirname%/dat*}
-    cp $DIR/${subpath}/elf/${filename}.host host.dat
-    if [ -e $DIR/${subpath}/elf/${filename}.begin_signature ]
+    cp $BASEDIR/${subpath}/elf/${filename}.host host.dat
+    if [ -e $BASEDIR/${subpath}/elf/${filename}.begin_signature ]
     then
-      cp $DIR/${subpath}/elf/${filename}.begin_signature begin_signature.dat
+      cp $BASEDIR/${subpath}/elf/${filename}.begin_signature begin_signature.dat
     fi
-    if [ -e $DIR/${subpath}/elf/${filename}.end_signature ]
+    if [ -e $BASEDIR/${subpath}/elf/${filename}.end_signature ]
     then
-      cp $DIR/${subpath}/elf/${filename}.end_signature end_signature.dat
+      cp $BASEDIR/${subpath}/elf/${filename}.end_signature end_signature.dat
     fi
-    if [ -e $DIR/${subpath}/ref/${filename}.reference_output ]
+    if [ -e $BASEDIR/${subpath}/ref/${filename}.reference_output ]
     then
-      cp $DIR/${subpath}/ref/${filename}.reference_output reference.dat
+      cp $BASEDIR/${subpath}/ref/${filename}.reference_output reference.dat
     fi
     obj_dir/Vsoc $CYCLES 2> /dev/null
     if [ -f "reference.dat" ]
