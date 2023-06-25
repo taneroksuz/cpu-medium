0294ea90d072ada9ba230970043bd9ebe341dc1e
Taner Öksüz <taner.oeksuez@outlook.de>
2023-06-25 10:32:28 +0200

    superscalar implemented.

--- verilog/execute_stage.sv
+++ verilog/execute_stage.sv
@@ -252,7 +252,6 @@ module execute_stage
 
     if ((v.stall | a.m.stall | v.clear) == 1) begin
       v.instr0.op = init_operation_complex;
-      v.instr1.op = init_operation_basic;
     end
 
     if (v.clear == 1) begin
