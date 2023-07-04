ea20d43226b7306958dbcdf92cf96e3d534721c9
Taner Öksüz <taner.oeksuez@outlook.de>
2023-07-04 20:46:10 +0200

    pipeline optimized.

--- verilog/hazard.sv
+++ verilog/hazard.sv
@@ -123,10 +123,14 @@ module hazard
     v.calc1.mul_op = v.instr1.mul_op;
     v.calc1.bit_op = v.instr1.bit_op;
     v.calc1.fpu_op = v.instr1.fpu_op;
-    
-    v.single = v.calc1.op.fload | v.calc1.op.fstore | v.calc1.op.fpu | v.calc1.op.fence | v.calc1.op.mret | v.calc1.op.wfi;
+
+    v.single = v.calc1.op.fence | v.calc1.op.mret | v.calc1.op.wfi;
 
     v.dual = ((v.calc0.op.load | v.calc0.op.store) & (v.calc1.op.load | v.calc1.op.store));
+    v.dual = v.dual | ((v.calc0.op.load | v.calc0.op.store) & (v.calc1.op.fload | v.calc1.op.fstore));
+    v.dual = v.dual | ((v.calc1.op.load | v.calc1.op.store) & (v.calc0.op.fload | v.calc0.op.fstore));
+    v.dual = v.dual | ((v.calc0.op.fload | v.calc0.op.fstore) & v.calc1.op.fpu);
+    v.dual = v.dual | ((v.calc1.op.fload | v.calc1.op.fstore) & v.calc0.op.fpu);
     v.dual = v.dual | (v.calc0.op.division & v.calc1.op.division);
     v.dual = v.dual | (v.calc0.op.mult & v.calc1.op.mult);
     v.dual = v.dual | (v.calc0.op.bitc & v.calc1.op.bitc);
