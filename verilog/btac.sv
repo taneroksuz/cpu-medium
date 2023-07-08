1d11d3508e57753c8f4242dd229c6d1e24ec9a2d
Taner Öksüz <taner.oeksuez@outlook.de>
2023-07-08 11:53:47 +0200

    pipeline optimized.

--- verilog/btac.sv
+++ verilog/btac.sv
@@ -140,27 +140,27 @@ module btac_ctrl
       btac_out.pred_npc = 0;
     end
 
-    if (btac_in.clear == 0 && btac_in.memory_taken == 1) begin
+    if (btac_in.clear == 0 && btac_in.memory_taken == 1 && (btac_in.upd_pc0 == btac_in.memory_tpc || btac_in.upd_pc1 == btac_in.memory_tpc)) begin
       v.taken = btac_in.memory_taken;
       v.taddr = btac_in.memory_taddr;
       v.tpc = btac_in.memory_tpc;
       v.tnpc = btac_in.memory_tnpc;
-    end else if (btac_in.clear == 0 && btac_in.execute_taken == 1) begin
+    end else if (btac_in.clear == 0 && btac_in.execute_taken == 1 && (btac_in.upd_pc0 == btac_in.execute_tpc || btac_in.upd_pc1 == btac_in.execute_tpc)) begin
       v.taken = btac_in.execute_taken;
       v.taddr = btac_in.execute_taddr;
       v.tpc = btac_in.execute_tpc;
       v.tnpc = btac_in.execute_tnpc;
-    end else if (btac_in.clear == 0 && btac_in.issue_taken == 1) begin
+    end else if (btac_in.clear == 0 && btac_in.issue_taken == 1 && (btac_in.upd_pc0 == btac_in.issue_tpc || btac_in.upd_pc1 == btac_in.issue_tpc)) begin
       v.taken = btac_in.issue_taken;
       v.taddr = btac_in.issue_taddr;
       v.tpc = btac_in.issue_tpc;
       v.tnpc = btac_in.issue_tnpc;
-    end else if (btac_in.clear == 0 && btac_in.decode_taken == 1) begin
+    end else if (btac_in.clear == 0 && btac_in.decode_taken == 1 && (btac_in.upd_pc0 == btac_in.decode_tpc || btac_in.upd_pc1 == btac_in.decode_tpc)) begin
       v.taken = btac_in.decode_taken;
       v.taddr = btac_in.decode_taddr;
       v.tpc = btac_in.decode_tpc;
       v.tnpc = btac_in.decode_tnpc;
-    end else if (btac_in.clear == 0 && btac_in.fetch_taken == 1) begin
+    end else if (btac_in.clear == 0 && btac_in.fetch_taken == 1 && (btac_in.upd_pc0 == btac_in.fetch_tpc || btac_in.upd_pc1 == btac_in.fetch_tpc)) begin
       v.taken = btac_in.fetch_taken;
       v.taddr = btac_in.fetch_taddr;
       v.tpc = btac_in.fetch_tpc;
