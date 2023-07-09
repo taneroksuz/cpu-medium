7de48752a6233c67625eb4a21178482b392ee05d
Taner Öksüz <taner.oeksuez@outlook.de>
2023-07-09 10:37:44 +0200

    pipeline optimized.

--- verilog/btac.sv
+++ verilog/btac.sv
@@ -19,11 +19,155 @@ package btac_wires;
     logic [95 : 0] rdata1;
   } btb_out_type;
 
+  typedef struct packed{
+    logic [0  : 0] wen;
+    logic [2  : 0] waddr;
+    logic [2  : 0] raddr;
+    logic [95 : 0] wdata;
+  } btac_pred_in_type;
+
+  typedef struct packed{
+    logic [95 : 0] rdata;
+  } btac_pred_out_type;
+
+  typedef struct packed{
+    logic [0  : 0] rden;
+    logic [0  : 0] wren;
+    logic [95 : 0] wdata;
+    logic [0  : 0] clear;
+  } btac_fifo_in_type;
+
+  typedef struct packed{
+    logic [95 : 0] rdata;
+    logic [0  : 0] ready;
+  } btac_fifo_out_type;
+
 endpackage
 
 import configure::*;
 import wires::*;
 import btac_wires::*;
+
+module btac_pred
+(
+  input logic clock,
+  input btac_pred_in_type btac_pred_in,
+  output btac_pred_out_type btac_pred_out
+);
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  logic [95 : 0] btac_pred_array[0:7] = '{default:'0};
+
+  assign btac_pred_out.rdata = btac_pred_array[btac_pred_in.raddr];
+
+  always_ff @(posedge clock) begin
+    if (btac_pred_in.wen == 1) begin
+      btac_pred_array[btac_pred_in.waddr] <= btac_pred_in.wdata;
+    end
+  end
+
+endmodule
+
+module btac_fifo
+(
+  input logic reset,
+  input logic clock,
+  output btac_pred_in_type btac_pred_in,
+  input btac_pred_out_type btac_pred_out,
+  input btac_fifo_in_type btac_fifo_in,
+  output btac_fifo_out_type btac_fifo_out
+);
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  typedef struct packed{
+    logic [2:0] waddr;
+    logic [2:0] raddr;
+    logic [0:0] wren;
+    logic [0:0] rden;
+    logic [0:0] oflow;
+  } reg_type;
+
+  parameter reg_type init_reg = '{
+    waddr : 0,
+    raddr : 0,
+    wren : 0,
+    rden : 0,
+    oflow : 0
+  };
+
+  reg_type r,rin;
+  reg_type v;
+
+  always_comb begin
+    v = r;
+
+    v.wren = 0;
+    if (btac_fifo_in.wren == 1) begin
+      if (v.oflow == 1 && v.waddr < v.raddr) begin
+        v.wren = 1;
+      end else if (v.oflow == 0) begin
+        v.wren = 1;
+      end
+    end
+
+    v.rden = 0;
+    if (btac_fifo_in.rden == 1) begin
+      if (v.oflow == 0 && v.raddr < v.waddr) begin
+        v.rden = 1;
+      end else if (v.oflow == 1) begin
+        v.rden = 1;
+      end
+    end
+
+    if (v.wren == 1) begin
+      if (&(v.waddr) == 1) begin
+        v.oflow = 1;
+        v.waddr = 0;
+      end else begin
+        v.waddr = v.waddr + 1;
+      end
+    end
+
+    if (v.rden == 1) begin
+      if (&(v.raddr) == 1) begin
+        v.oflow = 0;
+        v.raddr = 0;
+      end else begin
+        v.raddr = v.raddr + 1;
+      end
+    end
+
+    if (btac_fifo_in.clear == 1) begin
+      v.waddr = 0;
+      v.raddr = 0;
+      v.wren = 0;
+      v.rden = 0;
+      v.oflow = 0;
+    end
+
+    btac_pred_in.wen = v.wren;
+    btac_pred_in.waddr = v.waddr;
+    btac_pred_in.wdata = btac_fifo_in.wdata;
+
+    btac_pred_in.raddr = v.raddr;
+
+    btac_fifo_out.rdata = btac_pred_out.rdata;
+    btac_fifo_out.ready = v.rden;
+
+    rin = v;
+  end
+
+  always_ff @(posedge clock) begin
+    if (reset == 0) begin
+      r <= init_reg;
+    end else begin
+      r <= rin;
+    end
+  end
+
+endmodule
 
 module btb
 (
@@ -61,7 +205,9 @@ module btac_ctrl
   input btac_in_type btac_in,
   output btac_out_type btac_out,
   input btb_out_type btb_out,
-  output btb_in_type btb_in
+  output btb_in_type btb_in,
+  input btac_fifo_out_type btac_fifo_out,
+  output btac_fifo_in_type btac_fifo_in
 );
   timeunit 1ns;
   timeprecision 1ps;
@@ -130,6 +276,18 @@ module btac_ctrl
       v.wen = 0;
       v.waddr = 0;
       v.wdata = 0;
+    end
+
+    btac_fifo_in.wren = v.wen;
+    btac_fifo_in.wdata = v.wdata;
+
+    btac_fifo_in.rden = btac_in.upd_jal0 | btac_in.upd_branch0 | btac_in.upd_jal1 | btac_in.upd_branch1;
+
+    if (btac_fifo_out.ready == 1) begin
+      v.taken = 1;
+      v.taddr = btac_fifo_out.rdata[95:64];
+      v.tpc = btac_fifo_out.rdata[63:32];
+      v.tnpc = btac_fifo_out.rdata[31:0];
     end
 
     if (btac_in.stall == 0 && btac_in.clear == 0) begin
@@ -204,6 +362,8 @@ module btac_ctrl
     btb_in.waddr = v.waddr;
     btb_in.wdata = v.wdata;
 
+    btac_fifo_in.clear = 0;
+
     rin = v;
 
     btac_out.pred_maddr = r.maddr;
@@ -239,11 +399,33 @@ module btac
       btb_in_type btb_in;
       btb_out_type btb_out;
 
+      btac_pred_in_type btac_pred_in;
+      btac_pred_out_type btac_pred_out;
+      btac_fifo_in_type btac_fifo_in;
+      btac_fifo_out_type btac_fifo_out;
+
       btb btb_comp
       (
         .clock (clock),
         .btb_in (btb_in),
         .btb_out (btb_out)
+      );
+
+      btac_pred btac_pred_comp
+      (
+        .clock (clock),
+        .btac_pred_in (btac_pred_in),
+        .btac_pred_out (btac_pred_out)
+      );
+
+      btac_fifo btac_fifo_comp
+      (
+        .reset (reset),
+        .clock (clock),
+        .btac_pred_in (btac_pred_in),
+        .btac_pred_out (btac_pred_out),
+        .btac_fifo_in (btac_fifo_in),
+        .btac_fifo_out (btac_fifo_out)
       );
 
       btac_ctrl btac_ctrl_comp
@@ -253,7 +435,9 @@ module btac
         .btac_in (btac_in),
         .btac_out (btac_out),
         .btb_in (btb_in),
-        .btb_out (btb_out)
+        .btb_out (btb_out),
+        .btac_fifo_in (btac_fifo_in),
+        .btac_fifo_out (btac_fifo_out)
       );
 
     end else begin
