8f8626643c3d61cce3b47289dd7bd944cca514d0
Taner Öksüz <taner.oeksuez@outlook.de>
2024-06-15 07:19:46 +0200

    top unit added.

---
+++ verilog/rtl/top.sv
@@ -0,0 +1,51 @@
+import configure::*;
+
+module top(
+  input  logic reset,
+  input  logic clock,
+  input  logic clock_slow,
+  input  logic uart_rx,
+  output logic uart_tx
+);
+
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  logic [0  : 0] uart_valid;
+  logic [0  : 0] uart_instr;
+  logic [31 : 0] uart_addr;
+  logic [31 : 0] uart_wdata;
+  logic [3  : 0] uart_wstrb;
+  logic [31 : 0] uart_rdata;
+  logic [0  : 0] uart_ready;
+
+  soc soc_comp
+  (
+    .reset (reset),
+    .clock (clock),
+    .clock_slow (clock_slow),
+    .uart_valid (uart_valid),
+    .uart_instr (uart_instr),
+    .uart_addr (uart_addr),
+    .uart_wdata (uart_wdata),
+    .uart_wstrb (uart_wstrb),
+    .uart_rdata (uart_rdata),
+    .uart_ready (uart_ready)
+  );
+
+  uart uart_comp
+  (
+    .reset (reset),
+    .clock (clock),
+    .uart_valid (uart_valid),
+    .uart_instr (uart_instr),
+    .uart_addr (uart_addr),
+    .uart_wdata (uart_wdata),
+    .uart_wstrb (uart_wstrb),
+    .uart_rdata (uart_rdata),
+    .uart_ready (uart_ready),
+    .uart_rx (uart_rx),
+    .uart_tx (uart_tx)
+  );
+
+endmodule
