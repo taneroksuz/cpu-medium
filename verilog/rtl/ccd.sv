ffc6ec705a9c15487117a12e286f4910c44c51ee
Taner Öksüz <taner.oeksuez@outlook.de>
2024-06-21 09:15:10 +0200

    cross clocked domain fixed.

--- verilog/rtl/ccd.sv
+++ verilog/rtl/ccd.sv
@@ -12,16 +12,16 @@ module ccd
   input  logic [0  : 0] memory_valid,
   input  logic [0  : 0] memory_instr,
   input  logic [31 : 0] memory_addr,
-  input  logic [31 : 0] memory_wdata,
-  input  logic [3  : 0] memory_wstrb,
-  output logic [31 : 0] memory_rdata,
+  input  logic [63 : 0] memory_wdata,
+  input  logic [7  : 0] memory_wstrb,
+  output logic [63 : 0] memory_rdata,
   output logic [0  : 0] memory_ready,
   output logic [0  : 0] memory_slow_valid,
   output logic [0  : 0] memory_slow_instr,
   output logic [31 : 0] memory_slow_addr ,
-  output logic [31 : 0] memory_slow_wdata,
-  output logic [3  : 0] memory_slow_wstrb,
-  input  logic [31 : 0] memory_slow_rdata,
+  output logic [63 : 0] memory_slow_wdata,
+  output logic [7  : 0] memory_slow_wstrb,
+  input  logic [63 : 0] memory_slow_rdata,
   input  logic [0  : 0] memory_slow_ready
 );
   timeunit 1ns;
@@ -34,7 +34,7 @@ module ccd
 
   logic [depth-1:0] count;
 
-  logic [31 : 0] memory_fast_rdata;
+  logic [63 : 0] memory_fast_rdata;
   logic [0  : 0] memory_fast_ready;
 
   initial begin
