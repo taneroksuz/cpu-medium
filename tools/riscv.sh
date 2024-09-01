351802683c5f7479884ed97aee5b576920bf2f1c
Taner Öksüz <taner.oeksuez@outlook.de>
2024-09-01 13:49:50 +0200

    script adjusted.

--- tools/riscv.sh
+++ tools/riscv.sh
@@ -5,10 +5,6 @@ RISCV=/opt/rv32imfdcb/
 ARCH=rv32imfdc_zba_zbb_zbc_zbs_zicsr_zifencei
 ABI=ilp32d
 
-GCC_VERSION="basepoints/gcc-15"
-NEWLIB_VERSION="newlib-4.4.0"
-BINTUILS_VERSION="binutils-2_42"
-
 if [ -d "$RISCV" ]; then
   sudo rm -rf $RISCV
 fi
@@ -16,42 +12,21 @@ fi
 sudo mkdir -p $RISCV
 sudo chown -R $USER:$USER $RISCV
 
-sudo apt-get -y install autoconf automake autotools-dev curl \
-                        python3 python3-pip libmpc-dev libmpfr-dev \
-                        libgmp-dev gawk build-essential bison flex \
-                        texinfo gperf libtool patchutils bc \
-                        zlib1g-dev libexpat-dev ninja-build git \
-                        cmake libglib2.0-dev libslirp-dev
+sudo apt-get -y install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev \
+                        libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf \
+                        libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake \
+                        libglib2.0-dev libslirp-dev
 
-if [ -d "gcc" ]; then
-  rm -rf gcc
-fi
-if [ -d "binutils" ]; then
-  rm -rf binutils
-fi
-if [ -d "newlib" ]; then
-  rm -rf newlib
-fi
-if [ -d "combined" ]; then
-  rm -rf combined
+if [ -d "riscv-gnu-toolchain" ]; then
+  rm -rf riscv-gnu-toolchain
 fi
 
-git clone --branch $GCC_VERSION --depth=1 https://github.com/gcc-mirror/gcc.git gcc
-git clone --branch $NEWLIB_VERSION --depth=1 https://github.com/bminor/newlib.git newlib
-git clone --branch $BINTUILS_VERSION --depth=1 https://github.com/bminor/binutils-gdb.git binutils
+git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git
 
-mkdir -p combined/build
+mkdir -p riscv-gnu-toolchain/build
+cd riscv-gnu-toolchain/build
 
-ln -r -s newlib/* combined/.
-ln --force -r -s binutils/* combined/.
-ln --force -r -s gcc/* combined/.
-
-cd combined/build
-
-../configure --target=riscv32-unknown-elf --enable-languages=c \
-             --disable-shared --disable-threads --disable-multilib \
-             --disable-gdb --disable-libssp --with-newlib \
-             --with-arch=$ARCH --with-abi=$ABI --prefix=$RISCV
+../configure --enable-llvm --disable-linux --with-arch=$ARCH --with-abi=$ABI --prefix=$RISCV
 
 make -j$(nproc)
 make install
