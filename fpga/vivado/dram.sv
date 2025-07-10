f800248bebbf6f5085ec3141760303449abc8eee
Taner Öksüz <taner.oeksuez@outlook.de>
2025-07-10 16:39:39 +0200

    dram controller optimized.

--- fpga/vivado/dram.sv
+++ fpga/vivado/dram.sv
@@ -30,40 +30,52 @@ module dram (
   logic [1:0]   rst_sync;
   logic         rstn;
 
-  logic         mem_ui_clk;
-  logic         mem_ui_rst;
+  logic         app_ui_clk;
+  logic         app_ui_rst;
   logic         calib_complete;
 
   typedef enum logic [2:0] { stIdle, stPreset, stSendData, stSetCmdRd, stSetCmdWr, stWaitCen } state_t;
 
   typedef struct packed {
-    logic [26:0]  mem_addr;
-    logic [2:0]   mem_cmd;
-    logic         mem_en;
-    logic [127:0] mem_wdf_data;
-    logic         mem_wdf_end;
-    logic [15:0]  mem_wdf_mask;
-    logic         mem_wdf_wren;
+    logic [0:0]   mem_valid;
+    logic [0:0]   mem_instr;
+    logic [1:0]   mem_mode;
+    logic [31:0]  mem_addr;
+    logic [63:0]  mem_wdata;
+    logic [7:0]   mem_wstrb;
+    logic [26:0]  app_addr;
+    logic [2:0]   app_cmd;
+    logic         app_en;
+    logic [127:0] app_wdf_data;
+    logic         app_wdf_end;
+    logic [15:0]  app_wdf_mask;
+    logic         app_wdf_wren;
     state_t       state;
   } register_in_type;
 
   register_in_type init_register_in = '{
+      mem_valid : 0,
+      mem_instr : 0,
+      mem_mode : 0,
       mem_addr : 0,
-      mem_cmd : 0,
-      mem_en : 0,
-      mem_wdf_data : 0,
-      mem_wdf_end : 0,
-      mem_wdf_mask : 0,
-      mem_wdf_wren : 0,
+      mem_wdata : 0,
+      mem_wstrb : 0,
+      app_addr : 0,
+      app_cmd : 0,
+      app_en : 0,
+      app_wdf_data : 0,
+      app_wdf_end : 0,
+      app_wdf_mask : 0,
+      app_wdf_wren : 0,
       state : stIdle
   };
 
   typedef struct packed {
-    logic         mem_rdy;
-    logic         mem_wdf_rdy;
-    logic [127:0] mem_rd_data;
-    logic         mem_rd_data_valid;
-    logic         mem_rd_data_end;
+    logic         app_rdy;
+    logic         app_wdf_rdy;
+    logic [127:0] app_rd_data;
+    logic         app_rd_data_valid;
+    logic         app_rd_data_end;
   } register_out_type;
 
   register_in_type r_in, rin_in, v_in;
@@ -73,45 +85,88 @@ module dram (
 
     v_in = r_in;
 
-    v_in.mem_wdf_wren = 1'b0;
-    v_in.mem_wdf_end  = 1'b0;
-    v_in.mem_en       = 1'b0;
-    v_in.mem_cmd      = 3'b000;
-
-    v_in.mem_wdf_data = {mem_in.mem_wdata,mem_in.mem_wdata,mem_in.mem_wdata,mem_in.mem_wdata};
-    v_in.mem_wdf_mask = 16'hFFFF;
-    v_in.mem_addr     = {mem_in.mem_addr[26:4],4'b0000};
-
     unique case (r_in.state)
-      stIdle     : if (mem_in.mem_valid && calib_complete) v_in.state = stPreset;
-      stPreset   : v_in.state = stSendData;
-      stSendData : if (r_out.mem_wdf_rdy) v_in.state = stSetCmdWr;
-      stSetCmdWr : if (r_out.mem_rdy)     v_in.state = stWaitCen;
-      stSetCmdRd : if (r_out.mem_rdy)     v_in.state = stWaitCen;
-      stWaitCen  : v_in.state = stIdle;
-      default    : v_in.state = stIdle;
+      stIdle     : begin
+        if (mem_in.mem_valid && calib_complete) begin
+          v_in.state = stPreset;
+        end
+      end
+      stPreset   : begin
+        if (|v_in.mem_wstrb) begin
+          v_in.state = stSendData;
+        end else begin
+          v_in.state = stSetCmdRd;
+        end
+      end
+      stSendData : begin
+        if (r_out.app_wdf_rdy) begin
+          v_in.state = stSetCmdWr;
+        end
+      end
+      stSetCmdWr : begin
+        if (r_out.app_rdy) begin
+          v_in.state = stWaitCen;
+        end
+      end
+      stSetCmdRd : begin
+        if (r_out.app_rdy) begin
+          v_in.state = stWaitCen;
+        end
+      end
+      stWaitCen  : begin
+        if (~mem_in.mem_valid) begin
+          v_in.state = stIdle;
+        end
+      end
+      default    : begin
+        v_in.state = stIdle;
+      end
     endcase
 
     unique case (r_in.state)
       stSendData : begin
-        v_in.mem_wdf_wren = 1'b1;
-        v_in.mem_wdf_end  = 1'b1;
+        v_in.app_wdf_wren = 1'b1;
+        v_in.app_wdf_end  = 1'b1;
       end
-      stSetCmdWr : begin
-        v_in.mem_en  = 1'b1;
-        v_in.mem_cmd = 3'b000;
+      default    : begin
+        v_in.app_wdf_wren = 1'b0;
+        v_in.app_wdf_end  = 1'b0;
       end
-      stSetCmdRd : begin
-        v_in.mem_en  = 1'b1;
-        v_in.mem_cmd = 3'b001;
-      end
-      default: ;
     endcase
 
+    unique case (r_in.state)
+      stSetCmdWr : begin
+        v_in.app_en  = 1'b1;
+        v_in.app_cmd = 3'b000;
+      end
+      stSetCmdRd : begin
+        v_in.app_en  = 1'b1;
+        v_in.app_cmd = 3'b001;
+      end
+      default    : begin
+        v_in.app_en  = 1'b0;
+        v_in.app_cmd = 3'b000;
+      end
+    endcase
+
+    if (r_in.state == stIdle) begin
+      if (mem_in.mem_valid) begin
+        v_in.mem_valid = mem_in.mem_valid;
+        v_in.mem_instr = mem_in.mem_instr;
+        v_in.mem_mode  = mem_in.mem_mode;
+        v_in.mem_addr  = mem_in.mem_addr;
+        v_in.mem_wdata = mem_in.mem_wdata;
+        v_in.mem_wstrb = mem_in.mem_wstrb;
+      end
+    end
+
     if (r_in.state == stPreset) begin
-      unique case (mem_in.mem_addr[3])
-        0 : v_in.mem_wdf_mask[7:0] <= ~mem_in.mem_wstrb;
-        1 : v_in.mem_wdf_mask[15:8] <= ~mem_in.mem_wstrb;
+      v_in.app_wdf_data = {v_in.mem_wdata,v_in.mem_wdata};
+      v_in.app_wdf_mask = 16'hFFFF;
+      v_in.app_addr     = {v_in.mem_addr[26:4],4'b0000};
+      unique case (v_in.mem_addr[3])
+        0 : v_in.app_wdf_mask[7:0] <= ~v_in.mem_wstrb;
+        1 : v_in.app_wdf_mask[15:8] <= ~v_in.mem_wstrb;
         default : ;
       endcase
     end
@@ -120,13 +175,14 @@ module dram (
 
     mem_out.mem_rdata = 0;
     mem_out.mem_ready = 0;
+    mem_out.mem_error = 0;
 
     if (r_in.state == stWaitCen) begin
-      if (r_out.mem_rd_data_valid && r_out.mem_rd_data_end) begin
+      if (r_out.app_rd_data_valid && r_out.app_rd_data_end) begin
         mem_out.mem_ready <= 1;
-        unique case (mem_in.mem_addr[3])
-          0 : mem_out.mem_rdata <= r_out.mem_rd_data[63:0];
-          1 : mem_out.mem_rdata <= r_out.mem_rd_data[127:64];
+        unique case (v_in.mem_addr[3])
+          0 : mem_out.mem_rdata <= r_out.app_rd_data[63:0];
+          1 : mem_out.mem_rdata <= r_out.app_rd_data[127:64];
           default : ;
         endcase
       end
@@ -134,15 +190,15 @@ module dram (
 
   end
 
-  always_ff @(posedge mem_ui_clk) begin
-    if (mem_ui_rst == 0) begin
+  always_ff @(posedge app_ui_clk) begin
+    if (app_ui_rst == 0) begin
       r_in <= init_register_in;
     end else begin
       r_in <= rin_in;
     end
   end
 
-  always_ff @(posedge mem_ui_clk) begin
+  always_ff @(posedge app_ui_clk) begin
     mem_in <= dram_in;
     dram_out <= mem_out;
   end
@@ -172,26 +228,26 @@ module dram (
     .ddr2_odt            (ddr2_odt),
     .sys_clk_i           (clk_200MHz_i),
     .sys_rst             (rstn),
-    .app_addr            (r_in.mem_addr),
-    .app_cmd             (r_in.mem_cmd),
-    .app_en              (r_in.mem_en),
-    .app_wdf_data        (r_in.mem_wdf_data),
-    .app_wdf_end         (r_in.mem_wdf_end),
-    .app_wdf_mask        (r_in.mem_wdf_mask),
-    .app_wdf_wren        (r_in.mem_wdf_wren),
-    .app_rd_data         (r_out.mem_rd_data),
-    .app_rd_data_end     (r_out.mem_rd_data_end),
-    .app_rd_data_valid   (r_out.mem_rd_data_valid),
-    .app_rdy             (r_out.mem_rdy),
-    .app_wdf_rdy         (r_out.mem_wdf_rdy),
+    .app_addr            (r_in.app_addr),
+    .app_cmd             (r_in.app_cmd),
+    .app_en              (r_in.app_en),
+    .app_wdf_data        (r_in.app_wdf_data),
+    .app_wdf_end         (r_in.app_wdf_end),
+    .app_wdf_mask        (r_in.app_wdf_mask),
+    .app_wdf_wren        (r_in.app_wdf_wren),
+    .app_rd_data         (r_out.app_rd_data),
+    .app_rd_data_end     (r_out.app_rd_data_end),
+    .app_rd_data_valid   (r_out.app_rd_data_valid),
+    .app_rdy             (r_out.app_rdy),
+    .app_wdf_rdy         (r_out.app_wdf_rdy),
     .app_sr_req          (1'b0),
     .app_ref_req         (1'b0),
     .app_zq_req          (1'b0),
     .app_sr_active       (),
     .app_ref_ack         (),
     .app_zq_ack          (),
-    .ui_clk              (mem_ui_clk),
-    .ui_clk_sync_rst     (mem_ui_rst),
+    .ui_clk              (app_ui_clk),
+    .ui_clk_sync_rst     (app_ui_rst),
     .init_calib_complete (calib_complete)
   );
 
